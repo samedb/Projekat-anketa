9500dc80d0a19f5c63810e0884d3b9d945cd0c34
samedb <41544886+samedb@users.noreply.github.com>
2020-01-11 17:26:59 +0100

    Ubacene slike i izmenjena pocetna stranica, ubaceni i tekstovi neki isto

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
                 <img src="img\SIVA.jpg" alt="First slide">
                 <div class="container">
                     <div class="carousel-caption text-left">
-                        <h1>Ovo ce da se popuni.</h1>
+                        <h1>Ulogujte se i popunite anketu.</h1>
                         <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Ulogujte se</a></p>
                     </div>
                 </div>
             </div>
@@ -23,9 +23,9 @@
                 <img src="img\SIVA.jpg" alt="Second slide">
                 <div class="container">
                     <div class="carousel-caption">
-                        <h1>Another example headline.</h1>
+                        <h1>Informacije o anketi</h1>
                         <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Jos informacija</a></p>
                     </div>
                 </div>
             </div>
@@ -33,9 +33,9 @@
                 <img src="img\SIVA.jpg" alt="Third slide">
                 <div class="container">
                     <div class="carousel-caption text-right">
-                        <h1>One more for good measure.</h1>
+                        <h1>Nemate nalog? Registurjte se!</h1>
                         <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Registrujte se</a></p>
                     </div>
                 </div>
             </div>
@@ -50,59 +50,49 @@
         </a>
     </div>
 
-
-    <!-- Marketing messaging and featurettes
-      ================================================== -->
-    <!-- Wrap the rest of the page in another container to center all the content. -->
-
     <div class="container marketing">
 
-        <!-- Three columns of text below the carousel -->
         <div class="row">
             <div class="col-lg-4">
-                <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                <h2>Heading</h2>
-                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                <img class="rounded-circle" src="img\upotreba.jpg" alt="Generic placeholder image" width="140" height="140">
+                <h2>Upotreba</h2>
+                <p>Računar je alat uz pomoć kojeg obavljamo određenu vrstu poslova. Kada kažemo “poslova” ne mislimo samo na opšte kancelarijske poslove, nego i na zabavu, čitanje, učenje, crtanje, projektovanje, komunikaciju, gledanje filmova, slušanje muzike, igranje video igara i sve ostalo za šta nam je inače i potreban računar.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">Jos informacija »</a></p>
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4">
-                <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                <h2>Heading</h2>
-                <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                <img class="rounded-circle" src="img\cpu.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Procesor</h2>
+                <p>Procesor (mikroprocesor, µP ili uP) je elektronska komponenta napravljena od minijaturnih tranzistora na jednom čipu (poluprovodničkom integralnom sklopu). Centralni procesor je srce svakog računara, iako centralni procesor (CPU - Central Processing Unit) nije jedini procesor, njega imaju grafička kartica (GPU), zvučna kartica i mnogi drugi delovi, ali pod imenom procesor najčešće se misli na centralni procesor (CPU).</p>
+                <p><a class="btn btn-secondary" href="#" role="button">Jos informacija »</a></p>
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4">
-                <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                <h2>Heading</h2>
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                <img class="rounded-circle" src="img\memorija.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>RAM</h2>
+                <p>Radna memorija (engl. Random-access memory — „memorija sa slučajnim pristupom“), verz. skr. RAM (engl. RAM), označava vrstu memorije koja je direktno adresibilna i njenom sadržaju se može pristupiti po proizvoljnoj lokaciji, a ne samo redom (sekvencijalno, kao kod traka). RAM dozvoljava da se podaci uzimaju direktno u nasumičnom redosledu. Najčešće se koristi u računarima kao primarna ili glavna memorija, mada to nije neophodno.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">Jos informacija »</a></p>
             </div><!-- /.col-lg-4 -->
         </div><!-- /.row -->
 
 
-        <!-- START THE FEATURETTES -->
-
         <hr class="featurette-divider">
 
         <div class="row featurette">
             <div class="col-md-7">
-                <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                <h2 class="featurette-heading">Operativni sistem </h2>
+                <p class="lead">Operativni sistem (OS) je skup programa i rutina odgovoran za kontrolu i upravljanje uređajima i računarskim komponentama kao i za obavljanje osnovnih radnji. Operativni sistem objedinjuje u celinu raznorodne delove računara i sakriva od krajnjeg korisnika detalje funkcionisanja ovih delova. Najpopularniji operativni sistemi su Windows, Mac i Linux.</p>
             </div>
             <div class="col-md-5">
-                <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16f6258f6a0%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16f6258f6a0%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.0234375%22%20y%3D%22261.25%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+                <img class="featurette-image img-fluid mx-auto" src="img\Windows-Mac-Linux.png" alt="OS" style="width: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16f6258f6a0%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16f6258f6a0%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.0234375%22%20y%3D%22261.25%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
             </div>
         </div>
 
         <hr class="featurette-divider">
 
         <div class="row featurette">
-            <div class="col-md-7 order-md-2">
-                <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-            </div>
-            <div class="col-md-5 order-md-1">
-                <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16f6258f6a4%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16f6258f6a4%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.0234375%22%20y%3D%22261.25%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 500px; height: 500px;">
+            <div class="col-12">
+                <h2 class="featurette-heading">Desktop, laptop ili tablet?</h2> <br>
+                <img class="featurette-image img-fluid mx-auto" src="img\tip.jpg" alt="Vrste racunara" data-holder-rendered="true" style="width: 100%; margin: auto;"> <br>
+                <p class="lead my-5">Postoje tri osnovne vrste računara, a to su: desktop računari, laptop računari i tablet računari. Desktop računar najbolji izbor ukoliko Vam je potreban računar na duže staze jer se unutrašnje komponente ovog uređaja vremenom mogu zameniti boljim i snažnijim, a popravke su jednostavne i relativno jeftine. Laptop računari pružaju visok nivo mobilnosti, to jest možete ih poneti svuda sa sobom. Pod uslovom da im baterija nije u kvaru, mogu se koristiti i na mestima na kojima nema struje. Zbog svojih malih dimenzija neće zauzimati puno prostora. Kod ove vrste računara unutrašnje komponente nije moguće menjati, pa dobro razmislite koji ćete laptop računar izabrati. Tablet računari predstavljaju najmlađu generaciju računara i namenjeni su osobama koje će računar koristiti samo za čitanje, gledanje filmova, slušanje muzike, krstarenje internetom, komunikaciju sa prijateljima, pregled fotografija ili igranje video igara.</p>
             </div>
         </div>
 
@@ -110,16 +100,16 @@
 
         <div class="row featurette">
             <div class="col-md-7">
-                <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                <h2 class="featurette-heading">Popravka</h2>
+                <p class="lead">Gašenje računara bez ikakvog upozorenja može biti vrlo frustrirajuće, posebno ako ste u tom trenutku radili nešto što se nakon ponovnog pokretanja sistema nije sačuvalo. Ovaj problem može biti uzrokovan različitim razlozima poput pregrevanja, grešaka u hardveru, virusa, problema s operativnim sistemom… Ako ste dovoljno spretni i imate nešto znanja o tehnologiji, mozete i sami resiti ove „kvarove“.</p>
             </div>
             <div class="col-md-5">
-                <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16f6258f6a6%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16f6258f6a6%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.0234375%22%20y%3D%22261.25%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 500px; height: 500px;">
+                <img class="featurette-image img-fluid mx-auto" 
+            src="img\popravka.jpg" alt="Popravka" style="width: 500px;">
             </div>
         </div>
 
-        <!-- /END THE FEATURETTES -->
 
-    </div><!-- /.container -->
+    </div>
 
     <?php include "inc/footer.php" ?>
